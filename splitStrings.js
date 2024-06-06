5a36ea1fe846c16769b09051ee23788a6b7da038
AvionShea <avionsheadesigns@gmail.com>
2024-06-06 09:34:53 -0400

    daily code challenge

--- splitStrings.js
+++ splitStrings.js
@@ -6,3 +6,17 @@
     * 'abc' =>  ['ab', 'c_']
     * 'abcdef' => ['ab', 'cd', 'ef']
  */
+function solution(str) {
+    const letters = str.split('');
+    let text = '';
+    for (const letter of letters) {
+        text += letter;
+        if (text.replace(/ /g, '').length % 2 === 0)
+            letter !== letters[letters.length - 1] && (text += ' ');
+    }
+    if (str.length % 2 !== 0) text += '_';
+    console.log(text);
+    return (text === '' && []) || text.split(' ');
+}
+
+console.log(solution(''));
