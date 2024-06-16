760081953ed34cf58838277baf5222e4d704941e
AvionShea <avionsheadesigns@gmail.com>
2024-06-16 15:31:48 -0400

    daily code challenge

---
+++ Codewars/8 Kyu/JavaScript Challenges/simpleMultiplication.js
@@ -0,0 +1,5 @@
+/*
+    DESCRIPTION:
+    This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+const simpleMultiplication = (num) => (num % 2 === 0) ? (num * 8) : (num * 9)
