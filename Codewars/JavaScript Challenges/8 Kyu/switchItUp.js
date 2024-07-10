201ca28acfa5108202f9194253e0834832bfa7c1
AvionShea <avionsheadesigns@gmail.com>
2024-07-09 21:37:11 -0400

    refactor

--- Codewars/JavaScript Challenges/8 Kyu/switchItUp.js
+++ Codewars/JavaScript Challenges/8 Kyu/switchItUp.js
@@ -9,37 +9,24 @@ If your language supports it, try using a switch statement.
 */
 const switchItUp = (num) => {
     switch (num) {
-        case 0:
-            return "Zero";
+        case 0: return "Zero";
 
-        case 1:
+        case 1: return "One";
 
-            return "One";
+        case 2: return "Two";
 
-        case 2:
-            return "Two";
+        case 3: return "Three";
 
-        case 3:
+        case 4: return "Four";
 
-            return "Three";
+        case 5: return "Five";
 
-        case 4:
-            return "Four";
+        case 6: return "Six";
 
-        case 5:
+        case 7: return "Seven";
 
-            return "Five";
+        case 8: return "Eight";
 
-        case 6:
-            return "Six";
-
-        case 7:
-            return "Seven";
-
-        case 8:
-            return "Eight";
-
-        case 9:
-            return "Nine";
+        case 9: return "Nine";
     };
 };
