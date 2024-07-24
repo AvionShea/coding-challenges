20f1ed26ec6eb843e2d969106ca60d72200af6da
AvionShea <avionsheadesigns@gmail.com>
2024-07-24 19:06:06 -0400

    daily code challenge

---
+++ Codewars/JavaScript Challenges/8 Kyu/sumMixedArray.js
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+ const sumMix = (arr) => {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    let element = Number(arr[i]);
+    sum += element;
+  }
+  return sum;
+ };
