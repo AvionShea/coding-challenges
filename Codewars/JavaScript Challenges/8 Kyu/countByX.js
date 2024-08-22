387c52f53d8d6d41d2ea67e3b7e82699a4abe99f
AvionShea <avionsheadesigns@gmail.com>
2024-08-22 16:29:13 -0400

    daily code challenge

--- Codewars/JavaScript Challenges/8 Kyu/countByX.js
+++ Codewars/JavaScript Challenges/8 Kyu/countByX.js
@@ -10,10 +10,10 @@ Examples
 countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10]
 
-Parameters =
-Return =
-Example =
-Pseudocode =
+Parameters = a number and its multiple
+Return = an array with the multiples
+Example = countBy(2,5) === [2,4,6,8,10]
+Pseudocode = take in two parameters, create an empty array, loop through, push to the empty array
 */
 
 const countBy = (x, n) => {
