ce68d86e5f7023f4f8dce293d8bc6a724729a045
AvionShea <avionsheadesigns@gmail.com>
2024-10-10 15:18:39 -0400

    daily code challenge

---
+++ Codewars/JavaScript Challenges/8 Kyu/exclamationMarksSeries#1.js
@@ -0,0 +1,29 @@
+/*
+Description:
+Description:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi!!"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+
+take in str
+determine if the end (str.length - 1) is equal to "!"
+if true, split str, slice starting at the index 0
+return str with applicable exclamation mark removed
+*/
+
+function remove(str) {
+    if (str[str.length - 1] == "!") {
+        return str.split("").slice(0, str.split("").length - 1).join("");
+    };
+};
+
+console.log(remove("Hi"), "Hi")
+console.log(remove("Hi! Hi!"), "Hi! Hi")
+console.log(remove("!Hi!"), "!Hi")
+console.log(remove("Hi!"), "Hi")
