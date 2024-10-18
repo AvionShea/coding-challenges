b4f258aa75ea0617b63b6a8a67f4b857ccb8e03a
AvionShea <avionsheadesigns@gmail.com>
2024-10-18 08:28:14 -0400

    added link

--- Codewars/JavaScript Challenges/8 Kyu/countOfPositivesAndSumOfNegatives.js
+++ Codewars/JavaScript Challenges/8 Kyu/countOfPositivesAndSumOfNegatives.js
@@ -1,5 +1,7 @@
 /*
 Description:
+https://www.codewars.com/kata/576bb71bbbcf0951d5000044
+
 Given an array of integers.
 
 Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
