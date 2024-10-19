966c5eccbe5189a4e9d62c110cd669edcd6e4557
AvionShea <avionsheadesigns@gmail.com>
2024-10-19 19:47:28 -0400

    cosmetic change and added link

--- Codewars/JavaScript Challenges/8 Kyu/exclamationMarksSeries#1.js
+++ Codewars/JavaScript Challenges/8 Kyu/exclamationMarksSeries#1.js
@@ -1,5 +1,6 @@
 /*
-Description:
+https://www.codewars.com/kata/57fae964d80daa229d000126
+
 Description:
 Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
 
