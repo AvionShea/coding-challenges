a80a6779ce60de33b2b79f975ad33f51d41e902d
AvionShea <avionsheadesigns@gmail.com>
2024-10-24 14:41:09 -0400

    added link

--- Codewars/JavaScript Challenges/8 Kyu/removeNExclamationMarks.js
+++ Codewars/JavaScript Challenges/8 Kyu/removeNExclamationMarks.js
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/57faf7275c991027af000679
+
 Description:
 Description:
 Remove n exclamation marks in the sentence from left to right. n is positive integer.
@@ -17,8 +19,8 @@ remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
 remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
 */
 
-function remove(s, n){
-  for(let i = 0; i < n; i++){
+function remove(s, n) {
+  for (let i = 0; i < n; i++) {
     s = s.replace("!", "");
   }
 
