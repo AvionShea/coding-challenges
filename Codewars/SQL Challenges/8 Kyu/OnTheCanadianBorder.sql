582b44d6ad1f570f38f380b77533029b59f3d01d
01000001 01110110 01101001 01101111 01101110 <avionsheadesigns@gmail.com>
2024-10-29 07:50:22 -0400

    Added Link

--- Codewars/SQL Challenges/8 Kyu/OnTheCanadianBorder.sql
+++ Codewars/SQL Challenges/8 Kyu/OnTheCanadianBorder.sql
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/590ba881fe13cfdcc20001b4
+
 Description:
 You are a border guard sitting on the Canadian border. You were given a list of travelers who have arrived at your gate today. You know that American, Mexican, and Canadian citizens don't need visas, so they can just continue their trips. You don't need to check their passports for visas! You only need to check the passports of citizens of all other countries!
 
