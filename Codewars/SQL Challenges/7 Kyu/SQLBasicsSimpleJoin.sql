48132988c138b78c4c74764e0e13baf9615b4072
01000001 01110110 01101001 01101111 01101110 <avionsheadesigns@gmail.com>
2024-11-22 22:36:17 -0500

    Added Link

--- Codewars/SQL Challenges/7 Kyu/SQLBasicsSimpleJoin.sql
+++ Codewars/SQL Challenges/7 Kyu/SQLBasicsSimpleJoin.sql
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/5802e32dd8c944e562000020
+
 Description:
 For this challenge you need to create a simple SELECT statement that will return all columns from the products table, and join to the companies table so that you can return the company name.
 products table schema
