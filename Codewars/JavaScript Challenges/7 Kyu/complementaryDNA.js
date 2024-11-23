14324a244cfd5611ecd18fee9b61311cf988b440
01000001 01110110 01101001 01101111 01101110 <avionsheadesigns@gmail.com>
2024-11-22 22:29:57 -0500

    Added Link

--- Codewars/JavaScript Challenges/7 Kyu/complementaryDNA.js
+++ Codewars/JavaScript Challenges/7 Kyu/complementaryDNA.js
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+
 Description:
 Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
