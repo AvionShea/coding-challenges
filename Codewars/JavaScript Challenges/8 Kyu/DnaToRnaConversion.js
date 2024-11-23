b095a815525380a59e49641e066a19c872265e9c
01000001 01110110 01101001 01101111 01101110 <avionsheadesigns@gmail.com>
2024-11-22 22:28:33 -0500

    Added Link

--- Codewars/JavaScript Challenges/8 Kyu/DnaToRnaConversion.js
+++ Codewars/JavaScript Challenges/8 Kyu/DnaToRnaConversion.js
@@ -1,4 +1,6 @@
-/* 
+/*
+https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+
 Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
 Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
