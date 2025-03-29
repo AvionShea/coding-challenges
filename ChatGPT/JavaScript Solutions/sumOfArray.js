16798db70618dc38bde23f812f88ff424ca1af82
Avion <avionsheadesigns@gmail.com>
2025-03-29 19:51:42 -0400

    ChatGPT coding challenge - Sum of an Array

---
+++ ChatGPT/JavaScript Solutions/sumOfArray.js
@@ -0,0 +1,26 @@
+/* 
+First Challenge (Simple)
+Problem:
+Write a function that takes an array of numbers and returns the sum of all the elements.
+
+Example:
+js
+
+Input: [1, 2, 3, 4, 5]  
+Output: 15
+
+Requirements:
+You must write the solution in either JavaScript or Java.
+
+The function should work for both positive and negative numbers.
+
+The function should return 0 if the array is empty.
+*/
+
+function sumOfArray(arr) {
+    if (arr.length === 0) {
+        return 0;
+    } else {
+        return arr.reduce((a, b) => a + b, 0);
+    }
+};
