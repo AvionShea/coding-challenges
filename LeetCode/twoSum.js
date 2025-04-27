a027d5298e92b2172f31c27931f0b19eced89e6b
Avion <avionsheadesigns@gmail.com>
2025-04-27 00:10:36 -0400

    added explanation of code

--- LeetCode/twoSum.js
+++ LeetCode/twoSum.js
@@ -16,16 +16,25 @@ return pairs that = sum
 */
 
 function twoSum(arrOfNums, targetNum) {
+    // storing seen numbers in a hashmap (object)
     const seenNums = {};
 
+    //iterate through each number in the array
     for (let i = 0; i < arrOfNums.length; i++) {
+        //stores the current number's index
         const currentNum = arrOfNums[i];
+
+        //determines the difference between target number and the current number (seeing what number is needed to equal target)
         const diff = targetNum - currentNum;
 
+        //checks to see if the second number needed is stored in the hashmap
         if (diff in seenNums) {
+
+            //if yes, return the seen number index and the current number index
             return [seenNums[diff], i];
         }
 
+        // Store the index of the current number
         seenNums[currentNum] = i;
     }
 };
