c64be24e81be16d911783323eda5be6a43970d28
Avion C. <83898002+AvionShea@users.noreply.github.com>
2025-08-17 18:06:24 -0400

    remove dupes from array challenge completed

--- Scrimba/js-interview-challenges.js
+++ Scrimba/js-interview-challenges.js
@@ -389,3 +389,35 @@ function countChars(str) {
 
 console.log(countChars("Peggy Porth"));
 console.log(countChars("Samuel Jackson"));
+
+/* Chef Mario's Recipe Book 
+Chef Mario was in the middle of writing his cookbook masterpiece
+when he spilled coffee on his keyboard! Now all his recipes have repeat
+ingredients.
+
+Help save Chef Mario's cookbook by writing a function that takes in an array 
+and returns a new array with all the duplicates removed. 
+
+Example input: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops", "🦄 unicorn", "🍭 lollipops"];
+Example output: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops"];
+*/
+
+const eggScrambleRecipe = [
+    "🥓 bacon",
+    "🥓 bacon",
+    "🍳 eggs",
+    "🫑 green peppers",
+    "🧀 cheese",
+    "🌶️ hot sauce",
+    "🥓 bacon",
+    "🥦 broccoli",
+    "🧀 cheese",
+    "🥦 broccoli",
+    "🌶️ hot sauce"
+]
+
+function removeDupesFromArray(arr) {
+    return [... new Set(arr)];
+}
+
+console.log(removeDupesFromArray(eggScrambleRecipe));
