0223dd8750d6da7200579488d46e9a8a7416ab47
Avion C. <83898002+AvionShea@users.noreply.github.com>
2025-08-17 18:03:39 -0400

    completed letter count challenge

--- Scrimba/js-interview-challenges.js
+++ Scrimba/js-interview-challenges.js
@@ -356,3 +356,36 @@ function removeDupeChars(chars) {
 };
 
 console.log(removeDupeChars(password));
+
+/* 
+How often do the letters in your name repeat? 
+
+Write a function that counts how many times each letter of your name
+occurs. Your function should take in your first and last name and return
+an object where the keys are each character in your name, and the value
+is how many times that character appears in your name. 
+
+Example input: "Peggy Porth"
+Example output: {p: 2, e: 1, g: 2, y: 1, o: 1, r: 1, t: 1, h: 1}
+
+Your function should NOT count spaces and should not be case sensitive (a
+lowercase t and a capital T should be considered the same character).
+
+*/
+
+function countChars(str) {
+    const lowercaseNoSpacesStr = str.toLowerCase().split(" ").join("");
+    const letterCount = {};
+    for (let i = 0; i < lowercaseNoSpacesStr.length; i++) {
+        let currentChar = lowercaseNoSpacesStr[i];
+        if (!letterCount[currentChar]) {
+            letter[currentChar] = 1
+        } else {
+            letterCount[currentChar]++
+        }
+    }
+    return letterCount;
+}
+
+console.log(countChars("Peggy Porth"));
+console.log(countChars("Samuel Jackson"));
